a8d9528f3e0d1e15fe3eb01e768129c72abed96a
Aaryuh <120733942+Aaryuh@users.noreply.github.com>
2026-07-10 22:12:38 +0100

    Add: longest-consecutive-sequence - submission-4

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-4.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length == 0){
+            return 0;
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+        for(int i: nums){
+            set.add(i);
+        }
+        
+        int longest = 0;
+        int length = 0;
+        for(int i : set){
+            if(!set.contains(i-1)){
+                length = 0;
+                while(set.contains(i + length)){
+                    length++;
+                }
+            }
+            longest = Math.max(longest, length);
+        }
+        return longest;
+    }
+}
